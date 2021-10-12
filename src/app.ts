bc7d94cdbaa71f2bc6a671c7561d2f052fe18fd1
imjonlam <jonathanlam.1994@gmail.com>
2021-10-12 07:23:41 -0400

    sending index.html url on express gets

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import path from "path";
 
-const prodURL = path.join(__dirname, "..", "..", "client", "public");
-const devURL = path.join(__dirname, "..", "..", "client", "public");
+const prodRootURL = path.join(__dirname, "..", "..", "client", "public");
+const devRootURL = path.join(__dirname, "..", "..", "client", "public");
 
 var express = require("express");
 var app = express();
 app.use(
-    express.static(process.env.NODE_ENV === "production" ? prodURL : devURL)
+    express.static(
+        process.env.NODE_ENV === "production" ? prodRootURL : devRootURL
+    )
 );
 
 module.exports = app;
