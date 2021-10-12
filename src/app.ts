e7ff1c88b711ab237dabc7df23595ebdd4e95415
imjonlam <jonathanlam.1994@gmail.com>
2021-10-12 06:37:37 -0400

    added express static page

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
+import path from "path";
+
 var express = require("express");
 var app = express();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "..", "client", "public")));
+} else {
+    app.use(
+        express.static(path.join(__dirname, "..", "..", "client", "public"))
+    );
+}
 
 module.exports = app;
