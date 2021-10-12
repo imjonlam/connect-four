8e3002b5aaebc1eb645a1269e85c286213be0f41
imjonlam <jonathanlam.1994@gmail.com>
2021-10-12 07:05:35 -0400

    sending index.html url on express gets

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import path from "path";
 
+console.log(__dirname);
+
 const prodURL = path.join(__dirname, "client", "public", "index.html");
 const devURL = path.join(
     __dirname,
@@ -15,9 +17,5 @@ var app = express();
 app.use(
     express.static(process.env.NODE_ENV === "production" ? prodURL : devURL)
 );
-
-app.get("*", function (req, res) {
-    res.sendFile(process.env.NODE_ENV === "production" ? prodURL : devURL);
-});
 
 module.exports = app;
